da127d44bbf5add259fb7d3d8aecd629e9fb5420
MarianPetlovanyi <petlyovanyy@gmail.com>
2022-04-10 22:42:33 +0300

    Added lab2

---
+++ ua/lviv/iot/lab2/manager/Main.java
@@ -0,0 +1,12 @@
+package ua.lviv.iot.lab2.manager;
+
+import ua.lviv.iot.lab2.manager.Gym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Gym gym1=new Gym();
+
+    }
+
+}
