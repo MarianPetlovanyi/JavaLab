2e714c74106a34855d003245678ea4a4c0ea0ad2
MarianPetlovanyi <petlyovanyy@gmail.com>
2022-04-21 21:06:18 +0300

    Added test (updated)

--- src/main/java/ua/lviv/iot/lab2/manager/Manager.java
+++ src/main/java/ua/lviv/iot/lab2/manager/Manager.java
@@ -7,22 +7,18 @@ import java.util.*;
 
 public class Manager {
     private Machine[] arrOfMachines;
-    private ArrayList<String> toDoList = new ArrayList<String>();
+    public ArrayList<String> getToDoList() {
+		return toDoList;
+	}
+
+	private ArrayList<String> toDoList = new ArrayList<String>();
     public Manager(Machine[] arrOfMachines){
-        this.arrOfMachines=arrOfMachines;
-        sortArrayByCash();
-        sortArrayByCashR();
-        sortArrayByCalories();
-        sortArrayByCaloriesR();
-        createExercisePlan();
-        printToDoList();
+        this.setArrOfMachines(arrOfMachines);
     }
     public void createExercisePlan(){
-        for (int i=0;i<arrOfMachines.length;i++) {
-            toDoList.add(arrOfMachines[i].doExercise());
+        for (int i=0;i<getArrOfMachines().length;i++) {
+            toDoList.add(getArrOfMachines()[i].doExercise());
         }
-    }
-    public void printToDoList(){
         System.out.println("To do list:");
         for (int i=0; i<toDoList.size();i++) {
             System.out.println(toDoList.get(i));
@@ -32,42 +28,48 @@ public class Manager {
     Comparator cashPerMinuteComparator= Comparator.comparing(Machine::getCashPerMinute);
     Comparator caloriesPerMinuteComparator = Comparator.comparing(Machine::getCaloriesPerMinute);
     public void sortArrayByCash(){
-        Arrays.sort(arrOfMachines, cashPerMinuteComparator);
+        Arrays.sort(getArrOfMachines(), cashPerMinuteComparator);
         System.out.println("Sorted by cash per minute: ");
-        for (int i = 0; i < arrOfMachines.length; i++)
+        for (int i = 0; i < getArrOfMachines().length; i++)
         {
-            System.out.println(arrOfMachines[i].toString());
+            System.out.println(getArrOfMachines()[i].toString());
         }
         System.out.println();
 
     }
     public void sortArrayByCashR(){
-        Arrays.sort(arrOfMachines, cashPerMinuteComparator.reversed());
+        Arrays.sort(getArrOfMachines(), cashPerMinuteComparator.reversed());
         System.out.println("Sorted(R) by cash per minute: ");
-        for (int i = 0; i < arrOfMachines.length; i++)
+        for (int i = 0; i < getArrOfMachines().length; i++)
         {
-            System.out.println(arrOfMachines[i].toString());
+            System.out.println(getArrOfMachines()[i].toString());
         }
         System.out.println();
 
     }
     public void sortArrayByCalories(){
-        Arrays.sort(arrOfMachines, caloriesPerMinuteComparator);
+        Arrays.sort(getArrOfMachines(), caloriesPerMinuteComparator);
         System.out.println("Sorted by calories per minute: ");
-        for (int i = 0; i < arrOfMachines.length; i++)
+        for (int i = 0; i < getArrOfMachines().length; i++)
         {
-            System.out.println(arrOfMachines[i].toString());
+            System.out.println(getArrOfMachines()[i].toString());
         }
         System.out.println();
 
     }
     public void sortArrayByCaloriesR(){
-        Arrays.sort(arrOfMachines, caloriesPerMinuteComparator.reversed());
+        Arrays.sort(getArrOfMachines(), caloriesPerMinuteComparator.reversed());
         System.out.println("Sorted(R) by calories per minute: ");
-        for (int i = 0; i < arrOfMachines.length; i++)
+        for (int i = 0; i < getArrOfMachines().length; i++)
         {
-            System.out.println(arrOfMachines[i].toString());
+            System.out.println(getArrOfMachines()[i].toString());
         }
         System.out.println();
     }
+	public Machine[] getArrOfMachines() {
+		return arrOfMachines;
+	}
+	public void setArrOfMachines(Machine[] arrOfMachines) {
+		this.arrOfMachines = arrOfMachines;
+	}
 }
