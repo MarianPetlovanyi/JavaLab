6998b4da39b8a4e8a3499486ffec1c37236d4117
MarianPetlovanyi <petlyovanyy@gmail.com>
2022-05-03 10:06:06 +0300

    Added lab6(Writer, csv, header, checkstyle)

--- src/main/java/ua/lviv/iot/lab2/hierarchy/Machine.java
+++ src/main/java/ua/lviv/iot/lab2/hierarchy/Machine.java
@@ -1,60 +1,72 @@
 package ua.lviv.iot.lab2.hierarchy;
 
 public abstract class Machine {
-    private int cashPerMinute;
-    private int caloriesPerMinute;
-    private int exerciseTime;
-    public int getExerciseTime() {
-        return exerciseTime;
-    }
+  private int cashPerMinute;
+  private int caloriesPerMinute;
+  private int exerciseTime;
 
-    public void setExerciseTime(int exerciseTime) {
-        this.exerciseTime = exerciseTime;
-    }
-    public String getName() {
-        return name;
-    }
+  public int getExerciseTime() {
+    return exerciseTime;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setExerciseTime(int exerciseTime) {
+    this.exerciseTime = exerciseTime;
+  }
 
-    private String name;
+  public String getName() {
+    return name;
+  }
 
-    public int getCashPerMinute() {
-        return cashPerMinute;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setCashPerMinute(int cashPerMinute) {
-        this.cashPerMinute = cashPerMinute;
-    }
+  private String name;
 
-    public int getCaloriesPerMinute() {
-        return caloriesPerMinute;
-    }
+  public int getCashPerMinute() {
+    return cashPerMinute;
+  }
 
-    public void setCaloriesPerMinute(int caloriesPerMinute) {
-        this.caloriesPerMinute = caloriesPerMinute;
-    }
-    protected Machine() {
-    	
-    }
+  public void setCashPerMinute(int cashPerMinute) {
+    this.cashPerMinute = cashPerMinute;
+  }
 
-    protected Machine(int cashPerMinute, int caloriesPerMinute, int exerciseTime,String name){
-        this.setCashPerMinute(cashPerMinute);
-        this.setCaloriesPerMinute(caloriesPerMinute);
-        this.setName(name);
-        this.setExerciseTime(exerciseTime);
-    }
-    public abstract String doExercise();
+  public int getCaloriesPerMinute() {
+    return caloriesPerMinute;
+  }
 
-    @Override
-    public String toString() {
-        return "Machine{" +
-                "cashPerMinute=" + cashPerMinute +
-                ", caloriesPerMinute=" + caloriesPerMinute +
-                ", exerciseTime=" + exerciseTime +
-                ", name='" + name + '\'' +
-                '}';
-    }
+  public void setCaloriesPerMinute(int caloriesPerMinute) {
+    this.caloriesPerMinute = caloriesPerMinute;
+  }
+  
+  protected Machine() {
+
+  }
+
+  protected Machine(int cashPerMinute, int caloriesPerMinute, int exerciseTime, String name) {
+    this.setCashPerMinute(cashPerMinute);
+    this.setCaloriesPerMinute(caloriesPerMinute);
+    this.setName(name);
+    this.setExerciseTime(exerciseTime);
+  }
+  
+  public abstract String doExercise();
+  
+  public String getHeaders() {
+    return "cashPerMinute;caloriesPerMinute;exerciseTime;name";
+  }
+
+  public String toCSV() {
+    return String.format("%d; %d; %d; %s", getCashPerMinute(), getCaloriesPerMinute(), getExerciseTime(), getName());
+  }
+
+  @Override
+  public String toString() {
+    return "Machine{" +
+        "cashPerMinute=" + cashPerMinute +
+        ", caloriesPerMinute=" + caloriesPerMinute +
+        ", exerciseTime=" + exerciseTime +
+        ", name='" + name + '\'' +
+        '}';
+  }
 }
